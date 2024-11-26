5c7dae8f0091542b5fe11e3d523538dbbc4dce7f
deshikmashudhi <deshikmashudhi@gmail.com>
2024-11-26 13:46:58 +0530

    root

---
+++ loops.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#write a file to check the user is having sudo access or not
+#linux command is id -u
+R="\e[31m" 
+G="\e[32m" 
+N="\e[0m" 
+
+if [ "$(id-u)" -eq 0 ]
+then
+    echo -e "$G you have root access"
+else
+    echo -e "$R you dont have roor access"
+fi
