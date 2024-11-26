9ef6b7f36bb06ed4a454fd7548521173e57f5af3
deshikmashudhi <deshikmashudhi@gmail.com>
2024-11-26 13:38:24 +0530

    ssde2

--- number.sh
+++ number.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #print if the number is greater than 10 or not
 NUMBER=$1
-R="\e[31m" red colour
-G="\e[32m" green colour
-N="\e[0m" normal colour
+R="\e[31m" 
+G="\e[32m" 
+N="\e[0m" 
 if [ $NUMBER -gt 10 ]  #gt is greater than number
 then
   echo -e "$G $NUMBER  $N is greater than 10"
