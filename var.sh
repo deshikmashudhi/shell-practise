19b7488d192f5f45e39ccdeece5dbda19dff1889
deshikmashudhi <deshikmashudhi@gmail.com>
2024-11-26 13:33:55 +0530

    sd

--- var.sh
+++ var.sh
@@ -2,4 +2,4 @@
 PERSON1=deshik
 PERSON2=shanmukha
 echo "$PERSON1 tinnava ra! $PERSON2 "
-echo "$PERSON2 ledu tinaled nik nachuty nuvu tinu $PERSON1 "
+echo " ledu tinaled nik nachuty nuvu tinu $PERSON1 "
