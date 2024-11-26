27125a41c6510f9ce5e41a4f36f77ffd5e9aef56
deshikmashudhi <deshikmashudhi@gmail.com>
2024-11-26 15:17:26 +0530

    usersudo

--- usercreate.sh
+++ usercreate.sh
@@ -6,12 +6,47 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
-read -p "enter the username to check: " $USERNAME 
-  
-if grep -q "^$USERNAME:" /etc/passwd;  #proceed -q quietly to check ^ ensure that available in starting
-then
-    echo -e  "$G user $USERNAME exists $N"
-else
-    echo -e "$G user $USERNAME does not exists $N"
+read -p "Enter the username to create:"  USERNAME
 
-fi
+# Call the function to create the user and give sudo access
+create_user "$USERNAME"
+
+# Call the function to validate sudo access
+validate_sudo_access "$USERNAME"
+
+create_user() {
+    USERNAME=$1
+
+    if id "$USERNAME" &>/dev/null;
+    then
+        echo -e " User $G $USERNAME $N already exists "
+    else
+        sudo useradd -m "$USERNAME"
+        if [ $? -eq 0 ]
+        then 
+            echo -e "user $G $USERNAME $N created"
+        else
+            echo -e "user $R $USERNAME $N not created"
+        exit 1
+        fi
+        sudo usermod -aG sudo "$USERNAME"
+        if [ $? -eq 0 ]
+        then
+            echo -e "user $G $USERNAME $N sudo acess granted"
+        else
+            echo -e "user $R $USERNAME $N sudo acess not granted"
+        fi
+    fi
+}
+
+valiate_sudo_access()
+{
+    USERNAME=$1
+    sudo -u "$USERNAME" sudo -l &>dev/null
+    if [ $? -eq 0 ]
+    then
+       echo -e "User $G $USERNAME has sudo access"
+    else
+      echo -e "User $R $USERNAME has no sudo access try to get sudo access"
+    fi
+}
