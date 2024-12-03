a1c2afc3770b0a7683056dc686096fc13bc3ecf9
deshikmashudhi <deshikmashudhi@gmail.com>
2024-12-03 14:16:11 +0530

    pract

--- disk.sh
+++ disk.sh
@@ -2,7 +2,7 @@
 echo "check the disk usage in linux system"
 disk_size=$(df -h|grep "/dev/xvda16" |awk '{print $5}'|cut -d '%' -f 1)
 echo "$disk_size%  disk is full"
-if [ $disk_size -gt 10 ];
+if [ $disk_size -gt 80 ];
 then
    echo "disk utilization is more than 10% free the space"
    exit 1
