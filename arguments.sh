09fefc7c03ae684e103d1407c6ba12b2a262809e
deshikmashudhi <deshikmashudhi@gmail.com>
2024-12-03 16:34:24 +0530

    pract

---
+++ arguments.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "this is my shell script: $0"
+echo "this is first argument passed to my script: $1"
+echo "this is second argument passed to my script: $2"
+echo "this is third argument passed to my script: $3"
+
--- deleteoldfiles.sh
+++ deleteoldfiles.sh
@@ -3,7 +3,6 @@ echo "this script to delete the old files which or older than 30 days"
 path=$1
 echo $path
 find $path -mtime +30 -delete
-
 if [ $? -eq 0 ];
 then
   echo "Files are deleted successfull"
