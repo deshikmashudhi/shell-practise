38c9a679e528dd2a1f670f514ab9a8304611926d
deshikmashudhi <deshikmashudhi@gmail.com>
2024-12-03 16:39:40 +0530

    pract

--- arguments.sh
+++ arguments.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 echo "this is my shell script: $0"
-echo "this is first argument passed to my script: $1"
-echo "this is second argument passed to my script: $2"
-echo "this is third argument passed to my script: $3"
 
 if [ $# -eq 0 ];
 then
    echo "please pass one qrgument"
    exit 1
 fi
+echo "this is first argument passed to my script: $1"
+echo "this is second argument passed to my script: $2"
+echo "this is third argument passed to my script: $3"
+
 
