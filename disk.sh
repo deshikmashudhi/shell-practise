85e4ed8e480fbd19262a3796d04b5a11505e83f9
deshikmashudhi <deshikmashudhi@gmail.com>
2024-12-03 14:09:27 +0530

    pract

---
+++ disk.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "check the disk uage in linux system"
+disk_size=$(df -h|grep "/dev/xvda16" |awk '{print $5}'|cut -d '%' -f1)
+echo "$disk_size of disk is full"
+if [ $disk_size -gt 10 ];
+then
+   echo "disk utilization is more than 10% free the space"
+else
+   echo "space is available"
+fi
