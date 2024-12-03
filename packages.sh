5915cc4790382ff34a0dc50ad52b664d7f05c243
deshikmashudhi <deshikmashudhi@gmail.com>
2024-12-03 16:54:38 +0530

    pract

--- packages.sh
+++ packages.sh
@@ -2,13 +2,13 @@
 #Author: deshik
 #installing multiple packages
 
-if [[ $# -eq 0 ]]
+if [ $# -eq 0 ]
 then
   echo "Usage: please provide software names as command line"
   exit 1
 fi
 
-if [[ $(id -u) -ne 0 ]]
+if [ $(id -u) -ne 0 ]
 then 
    echo "please run from the root use or wih sudo access"
    exit 2
