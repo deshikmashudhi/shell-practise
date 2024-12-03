709af28b18f81fdd4efb151adcef75c3a7aedd38
deshikmashudhi <deshikmashudhi@gmail.com>
2024-12-03 15:19:11 +0530

    pract

--- filesyste.sh
+++ filesyste.sh
@@ -2,6 +2,6 @@
 echo "this program get first 10 biggest in tthe file system passed via possitional argumnets"
 path="$1"
 echo $path
-du -ah $path | sort -hr | head -n 5 > /tmp/filesize.txt
+du -ah $path | sort -hr | head -n 10 > /tmp/filesize.txt
 echo "this is the list of big files in the file system $path"
 cat /tmp/filesize.txt
