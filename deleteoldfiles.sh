44e92b74ff77a6785e4cdec350d397e7b75e12a5
deshikmashudhi <deshikmashudhi@gmail.com>
2024-12-03 15:49:12 +0530

    pract

---
+++ deleteoldfiles.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "this script to delete the old files which or older than 30 days"
+path=$1
+echo $path
+find $path -mtime +30 -delete
+
+if [ $? -eq 0 ]
+then
+  echo "Files are deleted successfull"
+  exit 1
+fi
