af000a3e8939cf172a6b29fed3e65c89638cc2a6
deshikmashudhi <deshikmashudhi@gmail.com>
2024-12-03 17:15:47 +0530

    pract

--- packages.sh
+++ packages.sh
@@ -2,9 +2,6 @@
 #Author: deshik
 #installing multiple packages
 
-#!/bin/bash
-# Author: Deshik
-# Installing multiple packages
 
 # Check if at least one argument (software name) is provided
 if [ $# -eq 0 ]
@@ -20,6 +17,16 @@ then
    exit 2
 fi
 
+# Log directory setup
+LOGDIR="/home/centos/shellscript-logs"
+if [ ! -d "$LOGDIR" ]; then
+  echo "Log directory $LOGDIR does not exist. Creating it..."
+  mkdir -p "$LOGDIR"
+fi
+
+# Get current date
+DATE=$(date +%F)
+
 # Loop through each software passed as argument
 for software in "$@"
 do
@@ -29,13 +36,13 @@ do
        echo "$software is already installed."
    else
        echo "Installing software $software..."
-       sudo apt install $software -y &> /tmp/$software-install.log
+       sudo apt install $software -y &> "$LOGDIR/$software-$DATE.log"
 
        # Check if installation was successful
        if [ $? -eq 0 ]; then
            echo "Successfully installed $software."
        else
-           echo "Failed to install $software. Check the log file /tmp/$software-install.log for details."
+           echo "Failed to install $software. Check the log file $LOGDIR/$software-$DATE.log for details."
        fi
    fi
 done
